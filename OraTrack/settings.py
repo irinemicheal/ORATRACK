835310e24ae072fc9f7a02615897558025f4da9c
Irine Micheal <michealirine22@gmail.com>
2026-05-08 14:47:09 +0530

    Fix whitenoise static files storage

--- OraTrack/settings.py
+++ OraTrack/settings.py
@@ -124,8 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
